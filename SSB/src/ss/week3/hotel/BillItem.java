a3aebbe486c8d6c02672e054f9fd5dc5297db11a
Bart <bart@LaptopBdeb>
2015-11-29 14:51:54 +0100

    Added javadoc to BillItem, ran junit test on BillTest succesfully

--- SSB/src/ss/week3/hotel/BillItem.java
+++ SSB/src/ss/week3/hotel/BillItem.java
@@ -4,15 +4,27 @@ public class BillItem implements Bill.Item {
 	private String text;
 	private Double amount;
 	
+	/**
+	 * Constructor for Items that can be added to the bill.
+	 * @param text
+	 * @param amount
+	 */
+	
 	public BillItem(String text, Double amount) {
 		this.text = text;
 		this.amount = amount;
 	}
 	
+	/**
+	 * Returns the cost of an item.
+	 */
 	public double getAmount() {
 		return amount;
 	}
 	
+	/**
+	 * Returns the name of the item.
+	 */
 	public String toString() {
 		return text;
 	}
