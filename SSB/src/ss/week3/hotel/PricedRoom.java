ff8bee9b2bc0f331a865aa9f2a25e66edba289af
Pieter Jan <Pieter Jan@PieterJan>
2015-11-29 23:40:19 +0100

    Added JML to PricedRoom and PricedSafe

--- SSB/src/ss/week3/hotel/PricedRoom.java
+++ SSB/src/ss/week3/hotel/PricedRoom.java
@@ -2,21 +2,38 @@ package ss.week3.hotel;
 
 public class PricedRoom extends Room implements Bill.Item {
 
+	//------------------- Instance variables -------------------
+	
 	//private PricedSafe safe;
 	private Double roomPrice;
 	private int roomNumber;
 	
+	//------------------- Constructor -------------------
+	
+	/**
+	 * Creates the room price to the corresponding room number.
+	 * @param roomNumber the number of the room
+	 * @param roomPrice the price of the room
+	 * @param safePrice the price of the safe
+	 */
 	public PricedRoom(int roomNumber, Double roomPrice, Double safePrice) {
 		super(roomNumber, new PricedSafe(safePrice));
 		this.roomPrice = roomPrice;
-		this.roomNumber = roomNumber;
-		
+		this.roomNumber = roomNumber;	
 	}
 	
+	//------------------- Queries -------------------
+	
+	/**
+	 * Return the room number and the roomprice per night.
+	 */
 	public String toString() {
 		return "Room number: " + roomNumber + " costs " + roomPrice + "per Night";
 	}
 	
+	/**
+	 * Return the price of the room.
+	 */
 	public double getAmount(){
 		return roomPrice;
 	}
--- SSB/src/ss/week3/hotel/PricedSafe.java
+++ SSB/src/ss/week3/hotel/PricedSafe.java
@@ -2,16 +2,32 @@ package ss.week3.hotel;
 
 public class PricedSafe extends Safe implements Bill.Item {
 	
+	//------------------- Instance variables -------------------
+	
 	private Double price;
 	
+	//------------------- Constructor -------------------
+	
+	/**
+	 * Create the price of the safe.
+	 */
 	public PricedSafe(double price) {
 		this.price = price;
 	}
-
+	
+	//------------------- Queries -------------------
+	
+	/**
+	 * Returns the price of the corresponding safe, combined with an adequate text.
+	 */
 	public String toString() {
 		return "safe: " + price;
 	}
 	
+	/**
+	 * Returns the price of the safe.
+	 * @return price
+	 */
 	public double getAmount() {
 		return price;
 	}
