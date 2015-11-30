96ae2b005bf37a78eae66e68bd651ec456270b1d
Bart <bart@LaptopBdeb>
2015-11-30 15:17:25 +0100

    Added week4 first exercise

--- SSB/src/ss/week4/Exercises.java
+++ SSB/src/ss/week4/Exercises.java
@@ -1,12 +1,35 @@
 package ss.week4;
 
+import java.lang.Object;
+import java.lang.reflect.Array;
+
 public class Exercises {
     public static int countNegativeNumbers(int[] arr) {
         // TODO: implement, see exercise P-4.1
+    	int negativeNumbers = 0;
+    	for (int i = 0; i < arr.length; i++) {
+    		if (arr[i] < 0) {
+    			negativeNumbers ++;
+    		}
+    	}
+    	return negativeNumbers;    	
     }
 
     public static void reverseArray(int[] ints) {
         // TODO: implement, see exercise P-4.2
+    	int last;
+    	int first;
+    	int length;
+    	length = ints.length / 2;
+    	for (int i = 0; i < length; i++) {
+    		for (int j = ints.length; j > length ; j++) {
+    			first = ints[i];
+    			last = ints[j];
+    			set(ints, i, last);
+    			set(ints, j, first);
+    		}
+    	}
+    	
     }
 
     class SimpleList {
