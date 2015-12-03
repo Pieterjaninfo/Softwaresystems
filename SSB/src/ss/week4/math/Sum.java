aa2afe076bb94e46b8a0d337144ad7bf3d81bab2
Pieter Jan <Pieter Jan@PieterJan>
2015-12-03 10:46:56 +0100

    added ss.week4.math and changed ss.week4

---
+++ SSB/src/ss/week4/math/Sum.java
@@ -0,0 +1,36 @@
+package ss.week4.math;
+
+public class Sum implements Function {
+
+	//---------------- Instance variables ---------------------
+	
+	private Function f1;
+	private Function f2;
+	
+	//------------------- Constructor ------------------------
+	
+	public Sum(Function function1, Function function2) {
+		f1 = function1;
+		f2 = function2;
+	}
+	
+	//----------------- Queries ------------------------
+	
+	@Override
+	public double apply(double value) {
+		// TODO Auto-generated method stub
+		return f1.apply(value) + f2.apply(value);
+	}
+
+	@Override
+	public Function derivative() {
+		return new Sum(f1.derivative(), f2.derivative());
+	}
+	
+	@Override
+	public String toString() {
+		return String.format("The sum is: %s + %s.", f1.toString(), f2.toString());
+	}
+
+
+}
