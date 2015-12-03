f3cec8fefc6bf8d49d8e43afffd795072f7c717e
Bart <bart@LaptopBdeb>
2015-12-03 10:59:09 +0100

    created new branch and added functionality to linear product

--- SSB/src/ss/week4/math/LinearProduct.java
+++ SSB/src/ss/week4/math/LinearProduct.java
@@ -4,13 +4,18 @@ public class LinearProduct extends Product {
 
 	//---------------- Instance variables ---------------------
 	
-		private Function f1;
-		private Function f2;
+	private Function f1;
+	private Function f2;
 		
 		//------------------- Constructor ------------------------
 		
-		public LinearProduct(Function function1, Function function2) {
-			f1 = function1;
-			f2 = function2;
-		}
+	public LinearProduct(Constant function1, Function function2) {
+		super(function1, function2);
+		f1 = function1;
+		f2 = function2;
+	}
+	
+	public Function derivative() {
+		return new Product(f1, f2.derivative());
+	}
 }
