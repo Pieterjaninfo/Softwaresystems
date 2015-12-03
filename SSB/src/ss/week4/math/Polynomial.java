1e7c198d4c13e5b3432c36dda185350ba2136164
Pieter Jan <Pieter Jan@PieterJan>
2015-12-03 22:00:36 +0100

    added polynomial

---
+++ SSB/src/ss/week4/math/Polynomial.java
@@ -0,0 +1,29 @@
+package ss.week4.math;
+
+public class Polynomial implements Function {
+
+	private double[] constants;
+	private double result = 0;
+	private Function nextExponent;
+	private Function nextConstant;
+	private double[] derivativeConstants;
+	
+	public Polynomial(double[] constants) {
+		this.constants = constants;
+	}
+	
+	@Override
+	public double apply(double value) {
+		for (int i = 0; i < constants.length; i++) {
+			nextConstant = new Constant(constants[constants.length - i - 1]);
+			nextExponent = new LinearProduct(nextConstant, new Exponent(i));
+			result += nextExponent.apply(value);
+		}
+		return result;
+	}
+
+	@Override
+	public Function derivative() {
+		return null;
+	}
+}
