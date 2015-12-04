16e588876c548e9ac9e883876f60df59059b99c8
Pieter Jan <Pieter Jan@PieterJan>
2015-12-04 10:15:09 +0100

    edited integrandable and sum

--- SSB/src/ss/week4/math/Sum.java
+++ SSB/src/ss/week4/math/Sum.java
@@ -1,6 +1,6 @@
 package ss.week4.math;
 
-public class Sum implements Function {
+public class Sum implements Function, Integrandable {
 
 	//---------------- Instance variables ---------------------
 	
@@ -29,6 +29,27 @@ public class Sum implements Function {
 	@Override
 	public String toString() {
 		return String.format("The sum is: %s + %s.", f1.toString(), f2.toString());
+	}
+	
+	@Override
+	public Function integrand() {
+		
+		if (f1 instanceof Integrandable && f2 instanceof Integrandable) {
+			return new Sum(((Integrandable) f1).integrand(), ((Integrandable) f2).integrand());
+		}
+		if (f1 instanceof Integrandable) {
+			return ((Integrandable) f1).integrand();
+		}
+		
+		if (f2 instanceof Integrandable) {
+			return ((Integrandable) f2).integrand();
+		}
+		return null;
+		
+		
+		/*p1 = new LinearProduct(new Constant(1 / (constant + 1)), new Exponent(value));
+		p2 = new LinearProduct(new Constant(1 / (constant + 1)), new Exponent(value));
+		return new Sum(p1, p2);*/
 	}
 
 
