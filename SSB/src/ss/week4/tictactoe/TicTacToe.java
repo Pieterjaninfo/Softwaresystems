863a6c710b49aff904e2b3b54abac8da6c514bbb
Bart <bart@LaptopBdeb>
2015-12-09 11:20:18 +0100

    added functionality to tictactoe

--- SSB/src/ss/week4/tictactoe/TicTacToe.java
+++ SSB/src/ss/week4/tictactoe/TicTacToe.java
@@ -1,5 +1,7 @@
 package ss.week4.tictactoe;
 
+import java.util.Scanner;
+
 /**
  * Executable class for the game Tic Tac Toe. The game can be played against the
  * computer. Lab assignment Module 2
@@ -9,9 +11,26 @@ package ss.week4.tictactoe;
  */
 public class TicTacToe {
     public static void main(String[] args) {
-    	Player s0 = new HumanPlayer(args[0], Mark.OO);
-    	Player s1 = new HumanPlayer(args[1], Mark.XX);
-    	Game game = new Game(s0, s1);
-    	game.start();    	
+    	
+    	String[] answer = {"", ""};
+    	Scanner in = new Scanner(System.in);
+        do {
+            System.out.print("Please enter the name of player one.");
+            try {
+                answer[0] = in.hasNextLine() ? in.nextLine() : "";
+            } finally {}
+        } while (answer[0] == "");
+        
+        do {
+            System.out.print("Please enter the name of player two.");
+            try {
+                answer[1] = in.hasNextLine() ? in.nextLine() : "";
+            } finally {}
+        } while (answer[1] == "" && answer[0] != "");
+            	
+   		Player s0 = new HumanPlayer(answer[0], Mark.OO);
+   		Player s1 = new HumanPlayer(answer[1], Mark.XX);
+   		Game game = new Game(s0, s1);
+   		game.start();
     }
 }
