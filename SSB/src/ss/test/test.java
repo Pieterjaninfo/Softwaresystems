cd7b978e0e906059ed3a522a7a0e442de6c80a59
Bart <bart@LaptopBdeb>
2016-01-03 14:31:13 +0100

    finished getPasswordHash ran test succesfully

--- SSB/src/ss/test/test.java
+++ SSB/src/ss/test/test.java
@@ -2,4 +2,18 @@ package ss.test;
 
 public class test {
 
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String a = "abc: ww";
+		
+		String[] b = a.split(": ");
+		
+		System.out.printf("String a: %s, String b: %s and %s, string a na b: %s", a, b[0],b[1], a);
+
+	}
+
 }
--- SSB/src/ss/week6/dictionaryattack/DictionaryAttack.java
+++ SSB/src/ss/week6/dictionaryattack/DictionaryAttack.java
@@ -1,6 +1,15 @@
 package ss.week6.dictionaryattack;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.HashMap;
 import java.util.Map;
+
+import javax.xml.bind.DatatypeConverter;
 
 
 public class DictionaryAttack {
@@ -12,23 +21,45 @@ public class DictionaryAttack {
 	 * username: encodedpassword
 	 * 
 	 * After calling this method, the passwordMap class variable should be
-	 * filled withthe content of the file. The key for the map should be
+	 * filled with the content of the file. The key for the map should be
 	 * the username, and the password hash should be the content.
 	 * @param filename
 	 */
-	public void readPasswords(String filename) {
-		// To implement        
+	public void readPasswords(String filename) throws IOException{
+		BufferedReader in = null;
+		String[] splitInput;
+		String input = null;
+		
+		try {
+			in = new BufferedReader(new FileReader(filename));
+			input = in.readLine();
+		} catch (FileNotFoundException e){
+			System.out.println("file not found");
+		}
+		passwordMap = new HashMap();
+		if (input != null) {
+			splitInput = input.split(": ");
+			passwordMap.put(splitInput[0], splitInput[1]);
+		}
 	}
-
 	/**
 	 * Given a password, return the MD5 hash of a password. The resulting
 	 * hash (or sometimes called digest) should be hex-encoded in a String.
 	 * @param password
 	 * @return
+	 * @throws NoSuchAlgorithmException 
 	 */
 	public String getPasswordHash(String password) {
-    		// To implement
-    		return null;
+		MessageDigest md = null;
+		System.out.println(password + " ; " + password.getBytes());
+		try {
+			md = MessageDigest.getInstance("MD5");	
+		} catch (NoSuchAlgorithmException e) {
+			System.out.println("this algorithm doesn't exsist");
+		}
+		byte[] output = md.digest(password.getBytes());
+		System.out.println(output + " and: " + DatatypeConverter.printHexBinary(output) + " with; " + md.getAlgorithm());
+		return DatatypeConverter.printHexBinary(output).toLowerCase();
 	}
 	/**
 	 * Checks the password for the user the password list. If the user
